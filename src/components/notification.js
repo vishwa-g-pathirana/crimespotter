ab4a3570eacdb3fb5fffb03ac756d0ceb4d28a45
vishwa G pathirana <68497315+vishwa-g-pathirana@users.noreply.github.com>
2022-05-31 01:55:44 +0530

    final release

--- src/components/notification.js
+++ src/components/notification.js
@@ -1,61 +1,76 @@
-import React, { Fragment } from "react";
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Divider from '@mui/material/Divider';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Chip from '@mui/material/Chip';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import React, { Fragment, useEffect, useState } from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Chip from "@mui/material/Chip";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import { db } from "../firebase-config";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 
 const Notifications = () => {
-    return <Fragment>
-<List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      <ListItem alignItems="flex-start">
-      <ListItemAvatar>
-          <Avatar>N</Avatar>
-        </ListItemAvatar>
-      <Card sx={{ maxWidth: 345, }}>
+  const [message, setMessage] = useState([]);
+  const usersCollectionRef = collection(db, "alert");
 
-      <CardContent>
-        <Typography gutterBottom variant="h9" component="div">
-          Mr. Èxample name
-        </Typography>
-        <Chip icon={<LocationOnIcon />} label="Kiribathgoda" />
-      </CardContent>
-      <CardActions>
-        <Button size="small">Response</Button>
-
-      </CardActions>
-    </Card>
-        {/* <ListItemAvatar>
-          <Avatar>N</Avatar>
-        </ListItemAvatar>
-        <ListItemText
-          primary="Brunch this weekend?"
-          secondary={
-            <React.Fragment>
-              <Typography
-                sx={{ display: 'inline' }}
-                component="span"
-                variant="body2"
-                color="text.primary"
-              >
-                Ali Connors
-              </Typography>
-              {" — I'll be in your neighborhood doing errands this…"}
-            </React.Fragment>
-          }
-        /> */}
-      </ListItem>
-      <Divider variant="inset" component="li" />
-
-    </List>
+  const getMsg = async () => {
+    const data = await getDocs(usersCollectionRef);
+    setMessage(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  };
+  const deleteMsg = async (id) => {
+    const userDoc = doc(db, "alert", id);
+    await deleteDoc(userDoc);
+    getMsg();
+  };
+  useEffect(() => {
+    getMsg();
+  }, []);
+  setInterval(getMsg, 10000);
+  return (
+    <Fragment>
+      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+        {message.map((msg) => {
+          return (
+            <ListItem alignItems="flex-start">
+              <ListItemAvatar>
+                <Avatar>N</Avatar>
+              </ListItemAvatar>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardContent>
+                  <Typography gutterBottom variant="h9" component="div">
+                    {msg.name}
+                  </Typography>
+                  <Chip icon={<LocationOnIcon />} label={msg.location} />
+                </CardContent>
+                <CardActions>
+                  <Button
+                    onClick={() => {
+                      deleteMsg(msg.id);
+                    }}
+                    size="small"
+                  >
+                    Response
+                  </Button>
+                </CardActions>
+              </Card>
+            </ListItem>
+          );
+        })}
+        <Divider variant="inset" component="li" />
+      </List>
     </Fragment>
-}
+  );
+};
 export default Notifications;
